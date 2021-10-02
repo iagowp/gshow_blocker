320eb315f190f1f38f41a81872ec87b2f5247363
iagowp <iago.wp@hotmail.com>
2021-10-01 23:14:12 -0300

    update

--- remove_entertainment.js
+++ remove_entertainment.js
@@ -2,3 +2,6 @@ var elements = document.querySelectorAll('[data-tracking-action="entretenimento"
 elements.forEach((element) => {
   element.parentElement.removeChild(element);
 });
+
+var column = document.querySelector('.column-container-entretenimento');
+column.parentElement.removeChild(column);
