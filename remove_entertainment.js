260c76a2fb2c0b95a8b1be0d9830f2ee9e927cd5
iagowp <iago.wp@hotmail.com>
2021-11-01 13:17:35 -0300

    bugfix

--- remove_entertainment.js
+++ remove_entertainment.js
@@ -1,9 +1,9 @@
+var column = document.querySelector('.column-container-entretenimento');
+column.parentElement.removeChild(column);
+
 var elements = document.querySelectorAll('[data-tracking-action="entretenimento"]')
 var mainPosts = document.querySelectorAll('.theme-entretenimento')
-var allPosts = mainPosts.concat(elements);
+var allPosts = [...mainPosts, ...elements];
 allPosts.forEach((element) => {
   element.parentElement.removeChild(element);
 });
-
-var column = document.querySelector('.column-container-entretenimento');
-column.parentElement.removeChild(column);
